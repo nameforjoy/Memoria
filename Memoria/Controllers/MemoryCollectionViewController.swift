35c284848a9b56dd19df21389a7c2fb955ede6c5
Joyce <joyce.sclimaco@gmail.com>
2020-10-07 13:14:49 -0300

    Adiciona Memory Collection e Question VC

---
+++ Memoria/Controllers/MemoryCollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryCollectionViewController.swift
+//  Memoria
+//
+//  Created by Joyce Simão Clímaco on 07/10/20.
+//
+
+import Foundation
+import UIKit
+
+class MemoryCollectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Memoria/Controllers/QuestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionViewController.swift
+//  Memoria
+//
+//  Created by Joyce Simão Clímaco on 07/10/20.
+//
+
+import Foundation
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
