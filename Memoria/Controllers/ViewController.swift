4b842ca9576dda661852a406151eb3c6c8566d55
Luma Gabino <lumagabino@gmail.com>
2020-10-08 10:34:46 -0300

    removing cloudkit methods

--- Memoria/Controllers/ViewController.swift
+++ Memoria/Controllers/ViewController.swift
@@ -6,74 +6,16 @@
 //
 
 import UIKit
-import CloudKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var loremLabel: UILabel!
-    let privateDatabase = CKContainer.default().privateCloudDatabase
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.save()
-        //self.retrieve()
+        
         self.setupAccessibility()
     }
-    
-    func save() {
-        let title = "Olá"
-        
-        let record = CKRecord(recordType: "Memory")
-        
-        record.setValue(title, forKey: "title")
-        
-        self.privateDatabase.save(record) { (savedRecord, error) in
-            
-            if error == nil {
-                
-                print("Record Saved")
-                
-            } else {
-                
-                print("Record Not Saved")
-                print(error)
-                
-            }
-            
-        }
-    }
-    
-//    func retrieve() {
-//        let predicate = NSPredicate(value: true)
-//
-//        let query = CKQuery(recordType: "Memory", predicate: predicate)
-//        query.sortDescriptors = [NSSortDescriptor(key: "modificationDate", ascending: false)]
-//
-//        let operation = CKQueryOperation(query: query)
-//
-//        var titles = [String]()
-//
-//        operation.recordFetchedBlock = { record in
-//
-//            titles.append(record["title"]!)
-//            print(record)
-//
-//        }
-//
-//        operation.queryCompletionBlock = { cursor, error in
-//
-//            DispatchQueue.main.async {
-//
-//                print("Titles: \(titles)")
-//
-//            }
-//
-//        }
-//
-//
-//        privateDatabase.add(operation)
-//    }
-    
     
     /// Method for configuring accessibility
     /// Set font and size, then associate with the label as a dynamicFont
