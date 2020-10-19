8e5b3b904ee99801be7b01a525db2216a3ad303f
Joyce <joyce.sclimaco@gmail.com>
2020-10-19 12:22:32 -0300

    Escurece cores para contraste

--- Memoria/Xibs/IconTextButton/IconTextButtonView.swift
+++ Memoria/Xibs/IconTextButton/IconTextButtonView.swift
@@ -9,4 +9,9 @@ import UIKit
 
 class IconTextButtonView: UIView {
     
+    @IBOutlet weak var buttonBackground: UIView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    
+    
 }
