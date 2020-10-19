b4e96d49a06c42feb64093449c89b987b3b8d874
Joyce <joyce.sclimaco@gmail.com>
2020-10-19 11:11:32 -0300

    Coloca gradiente no botão e cria xibs

---
+++ Memoria/Extensions/UIColorHex.swift
@@ -0,0 +1,8 @@
+//
+//  UIColorHex.swift
+//  Memoria
+//
+//  Created by Joyce Simão Clímaco on 19/10/20.
+//
+
+import Foundation
---
+++ Memoria/Xibs/GradientButton/GradientButtonView.swift
@@ -0,0 +1,8 @@
+//
+//  GradientButtonView.swift
+//  Memoria
+//
+//  Created by Joyce Simão Clímaco on 19/10/20.
+//
+
+import Foundation
