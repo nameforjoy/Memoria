48e74e92c101004cdf5b00a27f2a4ddc1cc65404
Luma Gabino <lumagabino@gmail.com>
2020-10-22 08:41:30 -0300

    trying to save  image in icloud

--- Memoria/Controllers/ImageSelectionViewController.swift
+++ Memoria/Controllers/ImageSelectionViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CloudKit
 
 class ImageSelectionViewController: UIViewController {
     
@@ -21,10 +22,47 @@ class ImageSelectionViewController: UIViewController {
         guard let senderView = sender as? UIView else { return }
         self.imagePicker.present(from: senderView)
     }
+    
+    func saveImageBD() {
+        let privateDatabase = CKContainer.default().privateCloudDatabase
+        let record = CKRecord(recordType: "Detail")
+        
+        let image = getImageAsset(image: self.imageView.image!)
+
+        record.setValue(image, forKey: "image")
+        
+        privateDatabase.save(record) { (savedRecord, error) in
+
+            if error == nil {
+                print("Record Saved")
+                print(savedRecord?.object(forKey: "image") ?? "Nil")
+
+            } else {
+                print("Record Not Saved")
+                print(error ?? "Nil")
+                
+            }
+        }
+    }
+    
+    func getImageAsset(image: UIImage) -> CKAsset? {
+        let data = image.pngData()
+        if let url = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(NSUUID().uuidString+".dat") {
+            do {
+                try data?.write(to: url)
+            } catch let err as NSError {
+                print("Error! \(err)")
+            }
+            return CKAsset(fileURL: url)
+        }
+        return nil
+    }
+    
 }
 
 extension ImageSelectionViewController: ImagePickerDelegate {
     func didSelect(image: UIImage?) {
         self.imageView.image = image
+        self.saveImageBD()
     }
 }
