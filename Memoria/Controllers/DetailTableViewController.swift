0a053d464b2c6537b9d07ff9d020b411127450a7
Luma Gabino <lumagabino@gmail.com>
2020-10-27 10:56:38 -0300

    TableView with different cells

--- Memoria/Controllers/DetailTableViewController.swift
+++ Memoria/Controllers/DetailTableViewController.swift
@@ -17,8 +17,16 @@ class DetailTableViewController: UITableViewController {
         
         self.tableView.separatorStyle = .none
         
-        let nib = UINib.init(nibName: self.photoCellIdentifier, bundle: nil)
-        self.tableView.register(nib, forCellReuseIdentifier: self.photoCellIdentifier)
+        let nibTitle = UINib.init(nibName: self.titleSubtitleCellIdentifier, bundle: nil)
+        self.tableView.register(nibTitle, forCellReuseIdentifier: self.titleSubtitleCellIdentifier)
+        
+        let nibSubtitle = UINib.init(nibName: self.subtitleCellIdentifier, bundle: nil)
+        self.tableView.register(nibSubtitle, forCellReuseIdentifier: self.subtitleCellIdentifier)
+        
+        let nibPhoto = UINib.init(nibName: self.photoCellIdentifier, bundle: nil)
+        self.tableView.register(nibPhoto, forCellReuseIdentifier: self.photoCellIdentifier)
+        
+        self.navigationItem.title = "Conta pra mim!"
     }
 
     // MARK: - Table view data source
@@ -27,17 +35,37 @@ class DetailTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 4
     }
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.subtitleCellIdentifier, for: indexPath)
+            if let cellType = cell as? SubtitleCell {
+                return cellType
+            }
+        } else if indexPath.row == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.titleSubtitleCellIdentifier, for: indexPath)
+            if let cellType = cell as? TitleSubtitleCell {
+                return cellType
+            }
+        } else if indexPath.row == 2 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.titleSubtitleCellIdentifier, for: indexPath)
+            if let cellType = cell as? TitleSubtitleCell {
+                return cellType
+            }
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.photoCellIdentifier, for: indexPath)
+            if let cellType = cell as? PhotoCell {
+                return cellType
+            }
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: self.photoCellIdentifier, for: indexPath)
         if let cellType = cell as? PhotoCell {
-            if (indexPath.item)%2 == 1 {
-                cellType.imageSelected = UIImage(named: "photo")
-            }
             return cellType
         }
+
         return cell
     }
 
