5ea8c96c6e41a7c13d22f70f8bafccb42df1274a
Luma Gabino <lumagabino@gmail.com>
2020-10-27 09:22:53 -0300

    TableView for detail screen, xibs title and subtitle

---
+++ Memoria/Controllers/DetailTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailTableViewController.swift
+//  Memoria
+//
+//  Created by Luma Gabino Vasconcelos on 27/10/20.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+    let titleSubtitleCellIdentifier: String = "TitleSubtitleCell"
+    let subtitleCellIdentifier: String = "SubtitleCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.separatorStyle = .none
+        
+        let nib = UINib.init(nibName: self.titleSubtitleCellIdentifier, bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: self.titleSubtitleCellIdentifier)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.titleSubtitleCellIdentifier, for: indexPath)
+        if let cellType = cell as? TitleSubtitleCell {
+            return cellType
+        }
+        return cell
+    }
+
+}
