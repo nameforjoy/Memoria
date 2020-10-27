91bd3bc44b4c20956f5d8820313e31f90268697b
Luma Gabino <lumagabino@gmail.com>
2020-10-27 10:22:26 -0300

    photo xib

--- Memoria/Controllers/DetailTableViewController.swift
+++ Memoria/Controllers/DetailTableViewController.swift
@@ -10,29 +10,32 @@ import UIKit
 class DetailTableViewController: UITableViewController {
     let titleSubtitleCellIdentifier: String = "TitleSubtitleCell"
     let subtitleCellIdentifier: String = "SubtitleCell"
+    let photoCellIdentifier: String = "PhotoCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.separatorStyle = .none
         
-        let nib = UINib.init(nibName: self.subtitleCellIdentifier, bundle: nil)
-        self.tableView.register(nib, forCellReuseIdentifier: self.subtitleCellIdentifier)
+        let nib = UINib.init(nibName: self.photoCellIdentifier, bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: self.photoCellIdentifier)
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 5
     }
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.subtitleCellIdentifier, for: indexPath)
-        if let cellType = cell as? SubtitleCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.photoCellIdentifier, for: indexPath)
+        if let cellType = cell as? PhotoCell {
+            if (indexPath.item)%2 == 1 {
+                cellType.imageSelected = UIImage(named: "photo")
+            }
             return cellType
         }
         return cell
