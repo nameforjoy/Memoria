65c3032052da4cd44c23b0e8f2aee0acde677207
Luma Gabino <lumagabino@gmail.com>
2020-10-29 08:39:39 -0300

    What???

--- Memoria/Xibs/PhotoCell/PhotoCell.swift
+++ Memoria/Xibs/PhotoCell/PhotoCell.swift
@@ -13,19 +13,27 @@ class PhotoCell: UITableViewCell {
     var imageSelected: UIImage? {
         didSet {
             self.imageCellView?.image = imageSelected
+            if let image = self.imageSelected {
+                let screenSize: CGRect = UIScreen.main.bounds
+                self.imageCellView.frame = CGRect(x: 0, y: 0, width: self.imageCellView.frame.size.width, height: ajustImageHeight(image: image))
+            }
+            
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        self.imageSelected = UIImage(named: "photo2")
-        self.imageCellView.contentMode = .scaleAspectFill
         self.imageCellView?.image = imageSelected
         self.imageCellView.layer.cornerRadius = 30
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func ajustImageHeight(image: UIImage) -> CGFloat {
+        let newHeight = imageCellView.frame.width / ( image.size.width / image.size.height)
+        return newHeight
     }
 }
