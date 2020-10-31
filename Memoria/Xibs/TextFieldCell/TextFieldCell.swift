d7919eece62d786f5ee11ac6ef2b69f2a9ce7443
Joyce <joyce.sclimaco@gmail.com>
2020-10-31 00:05:36 -0300

    Text View Cell

---
+++ Memoria/Xibs/TextFieldCell/TextFieldCell.swift
@@ -0,0 +1,23 @@
+//
+//  TextFieldCell.swift
+//  Memoria
+//
+//  Created by Joyce Simão Clímaco on 30/10/20.
+//
+
+import UIKit
+
+class TextFieldCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
