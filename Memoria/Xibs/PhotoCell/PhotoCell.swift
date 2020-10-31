a6c538a6dfa213079e89b72d9448a4ebc8630085
Joyce <joyce.sclimaco@gmail.com>
2020-10-30 21:53:21 -0300

    Acerta PhotoCell.swift

--- Memoria/Xibs/PhotoCell/PhotoCell.swift
+++ Memoria/Xibs/PhotoCell/PhotoCell.swift
@@ -14,11 +14,6 @@ class PhotoCell: UITableViewCell {
     var imageSelected: UIImage? {
         didSet {
             self.imageCellView?.image = imageSelected
-            if let image = self.imageSelected {
-                let screenSize: CGRect = UIScreen.main.bounds
-                self.imageCellView.frame = CGRect(x: 0, y: 0, width: self.imageCellView.frame.size.width, height: ajustImageHeight(image: image))
-            }
-            
         }
     }
     
@@ -34,10 +29,5 @@ class PhotoCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-    }
-    
-    func ajustImageHeight(image: UIImage) -> CGFloat {
-        let newHeight = imageCellView.frame.width / ( image.size.width / image.size.height)
-        return newHeight
     }
 }
