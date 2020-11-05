12c209a7d52f1e380c4eb314f351683181b035ae
Beatriz <bivilin@gmail.com>
2020-11-05 08:45:57 -0300

    Caso de uso de memória já com ID e base do método para recuperar memórias

--- Memoria/Xibs/Services/MemoryServices.swift
+++ Memoria/Xibs/Services/MemoryServices.swift
@@ -9,7 +9,9 @@ import Foundation
 
 class MemoryServices {
 
-    static public func createMemory(title: String?, hasDate: Bool, timePassedBy: Int?, timeUnit: Calendar.Component?, description: String?) {
+    /// Method to organize information from UI to conform the database
+    // Case without existing ID
+    static public func create(title: String?, hasDate: Bool, timePassedBy: Int?, timeUnit: Calendar.Component?, description: String?) {
 
         var estimatedDate: Date?
 
@@ -22,5 +24,31 @@ class MemoryServices {
         let newMemory = Memory(title: title, description: description, hasDate: hasDate, date: estimatedDate)
 
         MemoryDAO.create(memory: newMemory)
+    }
+
+    /// Method to organize information from UI to conform the database
+    // Case with existing ID
+    static public func create(memoryId: UUID, title: String?, hasDate: Bool, timePassedBy: Int?, timeUnit: Calendar.Component?, description: String?) {
+
+        var estimatedDate: Date?
+
+        if hasDate {
+            estimatedDate = DateManager.getEstimatedDate(timePassedBy: timePassedBy, timeUnit: timeUnit)
+        } else {
+            estimatedDate = nil
+        }
+
+        let newMemory = Memory(memoryID: memoryId, title: title, description: description, hasDate: hasDate, date: estimatedDate)
+
+        MemoryDAO.create(memory: newMemory)
+    }
+
+
+    /// Method to organize data from database before returning to de UI
+    // Might be a good place to reorder the memory array by date or any other criteria
+    static public func findAll(completion: @escaping ([Memory]) -> Void) {
+        MemoryDAO.findAll { (retievedMemories) in
+            completion(retievedMemories)
+        }
     }
 }
