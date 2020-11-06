6fa5896ae657fc787f0c3361a8916f769729712a
Joyce <joyce.sclimaco@gmail.com>
2020-11-06 08:47:22 -0300

    Salva memória com o ID

--- Memoria/Controllers/TitleTableViewController.swift
+++ Memoria/Controllers/TitleTableViewController.swift
@@ -232,7 +232,11 @@ extension TitleTableViewController: GradientButtonCellDelegate {
     func gradientButtonCellAction() {
         
         // Save memory
-        let memory = Memory(title: self.memoryTitle, description: self.memoryDescription, hasDate: true, date: self.date)
+        guard let id: UUID = self.memoryID else {
+            print("Memory ID not found")
+            return
+        }
+        let memory = Memory(memoryID: id, title: self.memoryTitle, description: self.memoryDescription, hasDate: true, date: self.date)
         print(memory)
         MemoryDAO.create(memory: memory)
         
