a9e4cb59a7a2dc88f0e86a0e858f2c19c6bf3085
Luma Gabino <lumagabino@gmail.com>
2020-11-06 08:43:11 -0300

    delete collectionview files, and create tableview and cell

---
+++ Memoria/Xibs/MemoryBox/MemoryBoxTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoryBoxTableViewCell.swift
+//  Memoria
+//
+//  Created by Luma Gabino Vasconcelos on 06/11/20.
+//
+
+import UIKit
+
+class MemoryBoxTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
