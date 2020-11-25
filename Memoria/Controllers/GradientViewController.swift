c26631169e52e1c505000b0085bca34d788d3bc6
Joyce <joyce.sclimaco@gmail.com>
2020-11-25 10:08:28 -0300

    Create gradient view

---
+++ Memoria/Controllers/GradientViewController.swift
@@ -0,0 +1,69 @@
+//
+//  GradientViewController.swift
+//  Memoria
+//
+//  Created by Joyce Simão Clímaco on 25/11/20.
+//
+
+import UIKit
+
+class GradientViewController: UIViewController {
+    
+    @IBOutlet weak var gradientView: UIView!
+    @IBOutlet weak var textLabel: UIView!
+    
+    let gradient = CAGradientLayer()
+    var gradientSet = [[CGColor]]()
+    var currentGradient: Int = 0
+    
+    let gradientOne = UIColor(hexString: "58BFE0").cgColor // blue
+    let gradientTwo = UIColor(hexString: "8571BE").cgColor // purple
+    let gradientThree = UIColor(hexString: "5C5081").cgColor // dark purple
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        gradientSet.append([gradientOne, gradientTwo])
+        gradientSet.append([gradientTwo, gradientThree])
+        gradientSet.append([gradientThree, gradientOne])
+        
+        gradient.frame = self.view.bounds
+        gradient.colors = gradientSet[currentGradient]
+        gradient.startPoint = CGPoint(x:0, y:0)
+        gradient.endPoint = CGPoint(x:1, y:1)
+        gradient.drawsAsynchronously = true
+        self.gradientView.layer.addSublayer(gradient)
+        
+        self.animateGradient()
+    }
+    
+    func animateGradient() {
+        if currentGradient < gradientSet.count - 1 {
+            currentGradient += 1
+        } else {
+            currentGradient = 0
+        }
+        
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.delegate = self
+        gradientChangeAnimation.duration = 3.0
+        gradientChangeAnimation.toValue = gradientSet[currentGradient]
+        gradientChangeAnimation.fillMode = CAMediaTimingFillMode.forwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        self.gradient.add(gradientChangeAnimation, forKey: "colorChange")
+    }
+    
+}
+
+extension GradientViewController: CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if flag {
+            gradient.colors = gradientSet[currentGradient]
+            self.animateGradient()
+        }
+    }
+}
